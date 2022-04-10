92988974a6cf6814badd9f66ce9fde41ec920586
Alibh <alibh619@gmail.com>
2022-04-10 23:14:02 +0530

    added list concat operator and list reverse function

--- app/Main.hs
+++ app/Main.hs
@@ -9,6 +9,9 @@ main = do
   putStrLn (firstOrEmpty ["Hello", "World"])
   putStrLn (firstOrEmpty [])
   someFunc (firstOrEmpty ["Hello"])
+  print ([1, 2] +++ [4, 5, 6])
+  print (reverse2 [1, 2, 3, 4, 5])
+  print (maxmin1 [1, 2, 3, 4, 5])
 
 -- someFunc (firstOrEmpty ["Hello", "World"])
 -- firstOrEmpty
